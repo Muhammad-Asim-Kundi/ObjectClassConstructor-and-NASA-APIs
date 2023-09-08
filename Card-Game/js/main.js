04e35c219ce9e89ddd408afb485f1293ab932c49
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2023-09-08 16:09:20 +0500

    make another directory for Card Game using deck APIs

--- Card-Game/js/main.js
+++ Card-Game/js/main.js
@@ -1,28 +1,55 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+  let deckId = ''
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  // const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+fetch(`https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+  .then(res => res.json()) // parse response as JSON
+  .then(data => {
+    console.log(data)
+    deckId = data.deck_id
 
-  const url = `https://api.nasa.gov/planetary/apod?api_key=Qt7DHVezgUkkUjeIdUJSEpUqgbTStKBzH8En5Eyg&date=${choice}`
+  })
+  .catch(err => {
+      console.log(`error ${err}`)
+  });
+   
+  
+document.querySelector('button').addEventListener('click', drawTwo)
 
+function drawTwo(){
+  const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-        if(data.media_type = 'image'){
-          document.querySelector('img').src = data.url
-        }else if(data.media_type = 'video'){
-          document.querySelector('iframe').src = data.url
-        }
-        
-        document.querySelector('h2').innerText = data.date
-        document.querySelector('h3').innerText = data.explanation
+    .then(res => res.json())
+    .then(data =>{
+      console.log(data)
+      document.querySelector('#player1').src = data.cards[0].image
+      document.querySelector('#player2').src = data.cards[1].image  
 
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+      let player01Val = convertToNum(data.cards[0].value)
+      let player02Val = convertToNum(data.cards[1].value)
+      if(player01Val > player02Val){
+        document.querySelector('h3').innerText = 'Player 1 Wins the game!'
+      }else if(player01Val < player02Val){
+        document.querySelector('h3').innerText = 'Player 2 Wins the game!'
+      }else{
+        document.querySelector('h3').innerText = 'Game of waaar!'
+      }
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    });
+
+    function convertToNum(val){
+      if(val === 'ACE' ){
+        return 14
+      }else if(val === 'KING'){
+        return 13
+      }else if(val === 'QUEEN'){
+        return 12
+      }else if(val === 'JACK'){
+        return 11
+      }else{
+        return Number(val)
+      }
+    }
+
 }
 
