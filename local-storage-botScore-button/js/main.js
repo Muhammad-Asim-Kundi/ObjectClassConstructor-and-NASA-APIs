b936f0e43ec520bfcfdf2d7bc203fc4f6e660313
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2023-09-08 12:21:19 +0500

    make another directory of Card-Game

--- local-storage-botScore-button/js/main.js
+++ local-storage-botScore-button/js/main.js
@@ -1,3 +1,12 @@
-//Create a button that adds 1 to a botScore stored in localStorage 
+//Create a button that adds 1 to a botScore stored in localStorage
+if(!localStorage.getItem('botScore')){
+    localStorage.setItem('botScore', 0 )
+}
 
+document.querySelector('button').addEventListener('click', getAnothaOne)
+function getAnothaOne(){
+    let botScoreVal = Number(localStorage.getItem('botScore'))
+    botScoreVal += 1
+    localStorage.setItem( 'botScore',botScoreVal)
+}
 
