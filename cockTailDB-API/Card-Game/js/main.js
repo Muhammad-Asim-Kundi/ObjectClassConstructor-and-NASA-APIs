3f6b52b8953d443802b27e4e3a6802167b75403c
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2023-09-11 21:59:47 +0500

    make cocktail api working as searching cocktail by names

--- cockTailDB-API/Card-Game/js/main.js
+++ cockTailDB-API/Card-Game/js/main.js
@@ -1,12 +1,18 @@
-fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`)
+document.querySelector('button').addEventListener('click', find)
+
+function find(){
+  let drink = document.querySelector('input').value
+fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
   .then(res => res.json()) // parse response as JSON
   .then(data => {
     console.log(data)
-    document.querySelector('h1').innerText= drinks[2]
+    document.querySelector('h3').innerText= data.drinks[0].strDrink
+    document.querySelector('img').src= data.drinks[0].strDrinkThumb
+
   })
   .catch(err => {
       console.log(`error ${err}`)
   })
-   
+}
   
 // document.querySelector('button').addEventListener('click', drawTwo)
